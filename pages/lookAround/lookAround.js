ca9380be0b9e9d36fa8591487b1e57c92bdebe51
KJFantasy <tangrengang@foxmail.com>
2020-12-16 01:22:07 +0800

    enter

--- pages/lookAround/lookAround.js
+++ pages/lookAround/lookAround.js
@@ -1,74 +1,95 @@
 // pages/lookAround/lookAround.js
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-    colorOrigin: '#f2f2f2',
-    colorWhite: '#ffffff',
+    tabs: [
+      {
+        id: 0,
+        name: "推荐",
+        isActive: true,
+      },
+      {
+        id: 1,
+        name: "iPhone",
+        isActive: false,
+      },
+      {
+        id: 2,
+        name: "华为",
+        isActive: false,
+      },
+      {
+        id: 3,
+        name: "小米",
+        isActive: false,
+      },
+      {
+        id: 4,
+        name: "OPPO",
+        isActive: false,
+      },
+      {
+        id: 5,
+        name: "vivo",
+        isActive: false,
+      },
+      {
+        id: 6,
+        name: "荣耀",
+        isActive: false,
+      },
+      {
+        id: 7,
+        name: "一加",
+        isActive: false,
+      },
+      {
+        id: 8,
+        name: "魅族",
+        isActive: false,
+      },
+      {
+        id: 9,
+        name: "红米",
+        isActive: false,
+      },
+      {
+        id: 10,
+        name: "三星",
+        isActive: false,
+      },
+      {
+        id: 11,
+        name: "锤子",
+        isActive: false,
+      },
+      {
+        id: 12,
+        name: "中兴",
+        isActive: false,
+      },
+      {
+        id: 13,
+        name: "联想",
+        isActive: false,
+      },
+      {
+        id: 14,
+        name: "realme",
+        isActive: false,
+      },
+    ],
   },
-  col: function () {
-          let temp = 'colorOrigin'
-          // 更改
-          if (this.data.colorOrigin == "#f2f2f2") {
-            this.setData({
-              [temp]: "#ffffff"
-            })
-          } 
-        },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  handleItemChange(e) {
+    const { index } = e.detail;
+    let { tabs } = this.data;
+    tabs.forEach((v, i) =>
+      i === index ? (v.isActive = true) : (v.isActive = false)
+    );
+    this.setData({
+      tabs,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
