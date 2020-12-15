63e5e753877193e8912b5795d0a2f97d15b3d5bd
KJFantasy <tangrengang@foxmail.com>
2020-12-15 12:50:57 +0800

    enter

---
+++ components/left-tabs/left-tabs.js
@@ -0,0 +1,95 @@
+Component({
+  data: {
+    tabs: [
+      {
+        id: 0,
+        name: "推荐",
+        isActive: true,
+      },
+      {
+        id: 1,
+        name: "iPhone",
+        isActive: false,
+      },
+      {
+        id: 2,
+        name: "华为",
+        isActive: false,
+      },
+      {
+        id: 3,
+        name: "小米",
+        isActive: false,
+      },
+      {
+        id: 4,
+        name: "OPPO",
+        isActive: false,
+      },
+      {
+        id: 5,
+        name: "vivo",
+        isActive: false,
+      },
+      {
+        id: 6,
+        name: "荣耀",
+        isActive: false,
+      },
+      {
+        id: 7,
+        name: "一加",
+        isActive: false,
+      },
+      {
+        id: 8,
+        name: "魅族",
+        isActive: false,
+      },
+      {
+        id: 9,
+        name: "红米",
+        isActive: false,
+      },
+      {
+        id: 10,
+        name: "三星",
+        isActive: false,
+      },
+      {
+        id: 11,
+        name: "锤子",
+        isActive: false,
+      },
+      {
+        id: 12,
+        name: "中兴",
+        isActive: false,
+      },
+      {
+        id: 13,
+        name: "联想",
+        isActive: false,
+      },
+      {
+        id: 14,
+        name: "realme",
+        isActive: false,
+      },
+    ],
+  },
+  properties: {},
+  methods: {
+    handleItemTap(e) {
+      // console.log(e);
+      const { index } = e.currentTarget.dataset;
+      let { tabs } = this.data;
+      tabs.forEach((v, i) =>
+        i === index ? (v.isActive = true) : (v.isActive = false)
+      );
+      this.setData({
+        tabs,
+      });
+    },
+  },
+});
